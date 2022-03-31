0214ee27f6832f433929d03e5fef82a0a282efc6
Ahmad Nadeem <ahmadnadeem509@gmail.com>
2022-03-31 12:13:53 +0500

    added Images folder

---
+++ src/components/AboutSection.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+      <div>
+          <div className="description">
+              <div className="title">
+                  <div className="hide">
+                      <h2>We work to make</h2>
+                  </div>
+                  <div className="hide">
+                      <h2>your <span>dreams</span>come</h2>
+                  </div>
+                  <div className="hide">
+                      <h2>true.</h2>
+                  </div>
+              </div>
+              <p>Contact us for photography or videography ideasthe you have. We have professionals with amazing skills </p>
+              <button>Contact Us</button>
+          </div>
+          <div className="image">
+              <img src="" alt="" />
+          </div>
+    </div>
+  )
+}
+
+export default AboutSection
